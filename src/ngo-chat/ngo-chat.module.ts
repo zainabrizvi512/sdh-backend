9a2941f9b4632bccdebd680e5b8af6ec1423e193
Ali Rizvi <syedalisajjadrizvi72@gmail.com>
2026-01-21 15:06:03 +0500

    Committing Ngo Chat Module

--- src/ngo-chat/ngo-chat.module.ts
+++ src/ngo-chat/ngo-chat.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { NgoMessage } from './ngo-message.entity';
 import { NgoChatService } from './ngo-chat.service';
 import { NgoChatGateway } from './ngo-chat.gateway';
+import { User } from 'src/users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([NgoMessage])],
+  imports: [TypeOrmModule.forFeature([NgoMessage, User])],
   providers: [NgoChatService, NgoChatGateway],
 })
 export class NgoChatModule {}
--- src/ngo-chat/ngo-chat.service.ts
+++ src/ngo-chat/ngo-chat.service.ts
@@ -1,32 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { NgoMessage } from './ngo-message.entity';
 import { User } from '../users/user.entity';
 import { NGO } from '../ngo/ngo.entity';
+import { use } from 'passport';
 
 @Injectable()
 export class NgoChatService {
-  constructor(
-    @InjectRepository(NgoMessage) private msgRepo: Repository<NgoMessage>,
-  ) {}
+    constructor(
+        @InjectRepository(NgoMessage) private msgRepo: Repository<NgoMessage>,
+        @InjectRepository(User) private userRepo: Repository<User>
+    ) { }
 
-  async saveMessage(userId: string, ngoId: string, text: string) {
-    // We assume validation happened in the Gateway
-    const msg = this.msgRepo.create({
-      text,
-      sender: { id: userId } as User,
-      ngo: { id: ngoId } as NGO,
-    });
-    return await this.msgRepo.save(msg);
-  }
+    async saveMessage(userId: string, ngoId: string, text: string) {
+        const user = await this.userRepo.findOne({ where: { sub: userId } });
+        if (!user) throw new NotFoundException('User not found');
+        // We assume validation happened in the Gateway
+        const msg = this.msgRepo.create({
+            text,
+            sender: { id: user.id } as User,
+            ngo: { id: ngoId } as NGO,
+        });
+        return await this.msgRepo.save(msg);
+    }
 
-  async getRecentMessages(ngoId: string) {
-    return this.msgRepo.find({
-      where: { ngo: { id: ngoId } },
-      order: { createdAt: 'DESC' },
-      take: 50,
-      relations: ['sender'],
-    });
-  }
+    async getRecentMessages(ngoId: string) {
+        return this.msgRepo.find({
+            where: { ngo: { id: ngoId } },
+            order: { createdAt: 'DESC' },
+            take: 50,
+            relations: ['sender'],
+        });
+    }
 }
